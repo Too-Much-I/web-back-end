89badfd4aed0487cb9d083bd6852b229876cd7e8
. <ngsonging@naver.com>
2026-07-09 10:38:46 +0900

    feat: azure results 저장 방식 수정

--- src/main/java/web/tosunsaeng/domain/exams/dto/ExamResponseDTO.java
+++ src/main/java/web/tosunsaeng/domain/exams/dto/ExamResponseDTO.java
@@ -2,7 +2,6 @@ package web.tosunsaeng.domain.exams.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.*;
